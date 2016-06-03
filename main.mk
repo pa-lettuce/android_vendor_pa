eb147b4b664ee06cf560a565df52fad052bef936
Prasath Balakrishnan <bpras@codeaurora.org>
2016-06-03 08:01:00 -0500

    Telephony(MSIM): Add telephony-ext to boot jar path and PRODUCT_PACKAGES
    Include the telephony-ext jar library in PRODUCT_PACKAGES
    and in boot jar path.
    The telephony-ext jar is needed for MSIM manual provisioning
    and other telephony related functionalities to work.
    
    Change-Id: I049ec39e88ec734e3770a9439d4c7ba8949b4357

--- main.mk
+++ main.mk
@@ -163,6 +163,13 @@ PRODUCT_PACKAGES += \
     rcs_service_api \
     rcs_service_api.xml
 
+# Telephony packages
+PRODUCT_PACKAGES += \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # Bluetooth Audio (A2DP)
 PRODUCT_PACKAGES += libbthost_if
 
